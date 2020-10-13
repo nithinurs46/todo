fa90d5931f769114e39890ac7a6109a513375222
Nithin Urs <nithinurs.venugopalrajurs@arisglobal.com>
2020-10-13 19:19:58 +0530

    Added authentication for class level

--- springboot/ToDo/src/test/java/com/todo/ToDoApplicationTests.java
+++ springboot/ToDo/src/test/java/com/todo/ToDoApplicationTests.java
@@ -4,11 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,10 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,16 +27,14 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.todo.dao.TaskManagerDao;
-import com.todo.dao.UserRepository;
 import com.todo.forms.TaskForm;
-import com.todo.forms.UserForm;
-import com.todo.models.MyUserDetails;
 import com.todo.models.Response;
 import com.todo.service.MyUserDetailsService;
 import com.todo.service.TaskManagerService;
 
 @SpringBootTest
 @TestInstance(Lifecycle.PER_CLASS)
+@WithUserDetails(value="AB")
 class ToDoApplicationTests {
 
 	private MockMvc mockMvc;
@@ -61,10 +53,10 @@ class ToDoApplicationTests {
 	}
 
 	@Test
-	@WithUserDetails(value="AB")
+	//@WithUserDetails(value="AB")
 	public void createNewTaskTest() throws Exception { // test the controller
 		TaskForm task = new TaskForm();
-		task.setTaskName("testing113 app18");
+		task.setTaskName("testing114 app18");
 		task.setCompleted("true");
 		task.setDescription("description");
 		task.setTaskDate(new Date());
